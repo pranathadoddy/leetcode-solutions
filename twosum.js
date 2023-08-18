7625f3741233255270794c9b73944d0c1f63e97d
DODDY\doddy <doddy@qiik.io>
2023-08-18 21:44:32 +0800

    two sum

---
+++ twosum.js
@@ -0,0 +1,57 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+*/
+
+/* 
+brute force
+var twoSum = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+
+        for (let j = i + 1; j < nums.length; j++) {
+            let test = nums[i] + nums[j]
+            if (test == target) {
+                return [i, j];
+            }
+        }
+    }
+
+}; */
+
+
+var twoSum = function(nums, target){
+    let dict = {};
+    for (let i = 0; i < nums.length; i++) {
+        let value = target - nums[i];
+        if(dict.hasOwnProperty(value)){
+            return [i, dict[value]];
+        }
+        else{
+            dict[nums[i]] = i;
+        }
+    }
+
+    return [];
+}
+
+
+console.log(twoSum([2, 4, 11, 3], 6))
